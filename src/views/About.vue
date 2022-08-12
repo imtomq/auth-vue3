fe8fa25e361411aad8678f3332e39d85fd1caa2f
quien dev <quiendev@example.com>
2022-08-12 19:23:19 +0700

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>About Page</h1>
+</template>
+<script>
+export default {
+  name: "MyAbout",
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Home Page</h1>
+</template>
+<script>
+export default {
+  name: "MyHome",
+};
+</script>
